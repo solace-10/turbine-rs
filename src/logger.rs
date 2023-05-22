ef3218d3365267ed4594c87bd0d7a7b3c7180b38
Pedro Nunes <solace10@proton.me>
2023-05-22 16:07:26 +0100

    Initial commit.

---
+++ src/logger.rs
@@ -0,0 +1,31 @@
+
+use log::{SetLoggerError, LevelFilter, Metadata, Record, Level};
+
+// struct LogTarget {
+
+// }
+
+pub struct Logger {
+
+}
+
+static LOGGER: Logger = Logger{};
+
+pub fn init() -> Result<(), SetLoggerError> {
+    log::set_logger(&LOGGER)
+        .map(|()| log::set_max_level(LevelFilter::Info))
+}
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
